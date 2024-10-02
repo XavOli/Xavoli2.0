d9163496fc12f3bf6fe7579b0e54b7f8d09ed9f4
Xavier Olivares <xavoli2099@gmail.com>
2024-10-02 19:36:24 -0400

    navbar fixes, hero styling and scoll navbar transparency

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
+// Hamburger + Menu
 const btn=document.getElementById('menu-btn')
 const menu=document.getElementById('menu')
 const navbar=document.getElementById('navbar')
 const logo=document.getElementById('logo')
-
 btn.addEventListener('click', navToggle)
-
 function navToggle(){
     btn.classList.toggle('open')
     menu.classList.toggle('flex')
@@ -12,3 +11,17 @@ function navToggle(){
     navbar.classList.toggle('flex-col')
     logo.classList.toggle('hidden')
 }
+
+// Scroll transparency
+const navCont = document.getElementById('navCont')
+window.addEventListener("scroll", changeColor);
+function changeColor() {
+    const scroll = window.scrollY;
+    if (scroll >= 50) {
+        navCont.classList.add('scroll-nav');
+        console.log(true);
+    } else {
+        navCont.classList.remove('scroll-nav');
+        console.log(false);
+    }
+};
